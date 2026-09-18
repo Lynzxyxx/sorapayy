cc6f297333cb14916685a6fc92dfb86c58837f00
Lynzxyxx <capchabisnis@gmail.com>
2026-09-18 09:32:39 +0700

    sorapayy/components/Footer.js

---
+++ Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="container">
+        © {new Date().getFullYear()} SoraPay. Semua hak dilindungi. Panel layanan
+        media sosial terpercaya untuk kebutuhan promosi digital Anda.
+      </div>
+    </div>
+  );
+}
