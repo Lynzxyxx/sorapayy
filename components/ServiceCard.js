5a77942adf8bde4044530dfa8f1afffa7162866f
Lynzxyxx <capchabisnis@gmail.com>
2026-09-19 16:52:01 +0700

    Delete components directory

--- components/ServiceCard.js
+++
@@ -1,9 +0,0 @@
-export default function ServiceCard({ icon, title, desc }) {
-  return (
-    <div className="card">
-      <div className="icon-badge">{icon}</div>
-      <h3 style={{ margin: "0 0 8px" }}>{title}</h3>
-      <p style={{ color: "var(--text-dim)", fontSize: 14, lineHeight: 1.6, margin: 0 }}>{desc}</p>
-    </div>
-  );
-}
